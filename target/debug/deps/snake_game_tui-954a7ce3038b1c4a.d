c1bc12d5df5793f36de9059cf9f021cf27a9ba4a
Jackson Brim <jackson.cr.brim@gmail.com>
2024-02-12 21:58:20 -0600

    Adjust boost mode text

---
+++ target/debug/deps/snake_game_tui-954a7ce3038b1c4a.d
@@ -0,0 +1,12 @@
+/home/gracklejacks/snake-game/target/debug/deps/libsnake_game_tui-954a7ce3038b1c4a.rmeta: src/main.rs src/app.rs src/game.rs src/screen.rs Cargo.toml
+
+/home/gracklejacks/snake-game/target/debug/deps/snake_game_tui-954a7ce3038b1c4a.d: src/main.rs src/app.rs src/game.rs src/screen.rs Cargo.toml
+
+src/main.rs:
+src/app.rs:
+src/game.rs:
+src/screen.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
